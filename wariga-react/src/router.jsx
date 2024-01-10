e62aa0d96ed3a5675a4b8b4a547723934e2c6c30
Bisma Bendesa <bismabendesajaya@gmail.com>
2024-01-10 12:47:29 +0800

    setup project

---
+++ wariga-react/src/router.jsx
@@ -0,0 +1,45 @@
+import { Navigate, createBrowserRouter } from "react-router-dom";
+import Login from "./components/Login";
+import NotFound from "./components/NotFound";
+import DefaultLayout from "./layouts/DefaultLayout";
+import GuestLayout from "./layouts/GuestLayout";
+import Register from "./components/Register";
+import Dashboard from "./components/Dashboard";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <DefaultLayout/>,
+    children: [
+      {
+        path: '/', 
+        element: <Navigate to='/dashboard'/>
+      }, 
+      {
+        path: '/dashboard', 
+        element: <Dashboard/>
+      }
+    ]
+  },
+  {
+    path:'/',
+    element: <GuestLayout />, 
+    children: [
+      {
+        path: '/login',
+        element: <Login />
+      },
+      {
+        path : '/register',
+        element: <Register />
+      }
+    ]
+  }, 
+  {
+    path: '*', 
+    element: <NotFound/>
+  }
+  
+])
+
+export default router;
