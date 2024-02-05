a2b3ef67e0b6caaa217d40464091aec72631a3c7
GusSurya031 <suryadiyatmika31@gmail.com>
2024-02-05 23:26:55 +0800

    Feat: Introduce main fiture with simple page

--- wariga-react/src/layouts/GuestLayout.jsx
+++ wariga-react/src/layouts/GuestLayout.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import { Outlet } from "react-router-dom";
 
 export default function GuestLayout() {
-  return (
-    <div>
-      <Outlet />
-    </div>
-  )
+    return (
+        <div>
+            <Outlet />
+        </div>
+    );
 }
